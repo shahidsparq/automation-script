6820818a4fd8a2456f23f3a535047d44cb137bcb
shahid <shahid@sparq.ai>
2024-02-22 18:04:52 +0530

    changes code

--- src/test/java/trackorder/ButtonValidationTest.java
+++ src/test/java/trackorder/ButtonValidationTest.java
@@ -7,25 +7,26 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.junit.jupiter.api.Test;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.Test;
 
 
 public class ButtonValidationTest {
     @Test
     public void urlLoad() throws InterruptedException {
 //        WebDriverManager.chromedriver().browserVersion("121.0.6167.160").setup();
-    
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--headless");
-        options.addArguments("--disable-gpu");
-        options.addArguments("--window-size=1920,1080");
-        options.addArguments("--no-sandbox");
-        options.addArguments("--disable-dev-shm-usage");
+//
+//        ChromeOptions options = new ChromeOptions();
+//        options.addArguments("--headless");
+//        options.addArguments("--disable-gpu");
+//        options.addArguments("--window-size=1920,1080");
+//        options.addArguments("--no-sandbox");
+//        options.addArguments("--disable-dev-shm-usage");
 
-        WebDriver driver = new ChromeDriver(options);
-//        driver.manage().window().maximize();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
 
         driver.get("https://www.trackorder.app/");
@@ -40,12 +41,11 @@ public class ButtonValidationTest {
 
         WebElement feature = driver.findElement(By.xpath("//h2[text()=' Features ']"));
 
-        JavascriptExecutor js = (JavascriptExecutor) driver;
-        js.executeScript("arguments[0].scrollIntoView(true);", feature);
+        JavascriptExecutor js=(JavascriptExecutor)driver;
+        Thread.sleep(2000);
+        js.executeScript("arguments[0].scrollIntoView(false)", feature);
 
-        js.executeAsyncScript("window.scrollBy(0,1900)");
 
-        Thread.sleep(5000);
 
 
     }
