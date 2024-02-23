4bd5098e4dfab842bdabacca7b00cee3c8155aea
shahid <shahid@sparq.ai>
2024-02-23 16:14:14 +0530

    Add some line

--- src/test/java/sparq/viewDemoTest.java
+++ src/test/java/sparq/viewDemoTest.java
@@ -1,26 +1,43 @@
 package sparq;
 
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
 
+
+
 public class viewDemoTest {
 
     @Test
-   public  void viewDemoLink()
-    {
+   public  void viewDemoLink() throws InterruptedException {
         WebDriver driver= new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
 
         driver.get("https://sparq.ai");
 
+        WebElement viewDemoButton = driver.findElement(By.xpath("//a[text()=' View Demo ']"));
+        Actions actions= new Actions(driver);
 
+        actions.moveToElement(viewDemoButton).click(viewDemoButton).perform();
+        String title = driver.getTitle();
+        System.out.println(title);
+        if(title.contains("Sparq"))
+        {
+            System.out.println("The page is find fine.");
+        }
+        else
+            System.out.println("The page find is not that page.");
 
+        Thread.sleep(2000);
 
+        driver.quit();
     }
 
 }
